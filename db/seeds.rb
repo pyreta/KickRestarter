b6a8746b089acb4f6b2edc71e1f2add84e5fb768
Louis Abramson <pyreta@gmail.com>
2016-08-12 09:57:28 -0400

    Seed db

--- db/seeds.rb
+++ db/seeds.rb
@@ -25,7 +25,8 @@ louis = User.create(
   biography: "I was born on the top of a hill in the middle of Minneapolis.  It was here that I learned the true nature of potato chips and what it means to actually live on said potato chips.  Is it potato or potatoe?  Ask DanQuail, perhaps he shall have an answer for you.  Otherwise, just google it.  These days you don't need to ask questions, just look it up.  Facts are attainable by all (who have phones).  Goodbye brozef",
   location_id: new_york_city.id,
   url: "www.jollyband.com",
-  email: "pyreta@gmail.com"
+  email: "pyreta@gmail.com",
+  image: File.open('app/assets/images/shame.jpg')
 )
 
 louie = User.create(
@@ -34,7 +35,8 @@ louie = User.create(
   biography: "I'm a baby, so there's not much to say.",
   location_id: miami_city.id,
   url: "www.louisabramson.com",
-  email: "louis@louisabramson.com"
+  email: "louis@louisabramson.com",
+  image: File.open('app/assets/images/louie.jpg')
 )
 
 # Categories
@@ -309,21 +311,124 @@ lou_diamond_phillips = Campaign.create(
   category_id: film_and_video_category.id,
   goal: 1000000,
   end_date: "2032-08-04 15:42:49.94213",
+  image: File.open('app/assets/images/ldp.jpg')
+)
+
+running_man = Campaign.create(
+  video_url: "https://www.youtube.com/watch?v=P0FKzPfsxA4",
+  title: "The Running Man",
+  blurb: "Greatest dance of all time.  Strep pretending you're better than it.",
+  description: "Man what a fun dance.  Wanna do it more with me and other people?  Give us money."+ loremString,
+  author_id: louie.id,
+  category_id: dance_category.id,
+  goal: 145,
+  end_date: "2032-08-04 15:42:49.94213",
+)
+
+mullets_and_muscles = Campaign.create(
+  video_url: "https://www.youtube.com/watch?v=s8RktlR_0Js",
+  title: "Mullets and Muscles",
+  blurb: "Business in the front, party in the back, and security all around.",
+  description: "Those were the glory days.  When muscles and mullets came together in perfect harmony on the silver screen."+ loremString,
+  author_id: louis.id,
+  category_id: film_and_video_category.id,
+  goal: 1000000,
+  end_date: "2019-06-05 15:42:49.94213",
+  image: File.open('app/assets/images/mullet.jpg')
 )
 
 slap_bracelets = Campaign.create(
   video_url: "https://www.youtube.com/watch?v=0Nwnp51QV5s",
   title: "Bring Back Slap Bracelets",
-  blurb: "They're worth cutting your wrists over.",
+  blurb: "Be careful little Billy!  They're dangerous.",
   description: "It's all fun and games until the cheap cloth comes off and you have little kids taking turns ramming sharp pieces of metal into their flesh." + loremString,
   author_id: louie.id,
   category_id: fashion_category.id,
   goal: 450,
   end_date: "2018-01-31 00:00:00.0000",
-  # image: File.open('app/assets/images/peewee.jpg')
+  image: File.open('app/assets/images/slap-bracelets.jpg')
 )
 
-cheap_reward = Reward.create(title: "Cheapskate award!", description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf", campaign_id: slap_bracelets.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 5)
+jncos = Campaign.create(
+  video_url: "https://www.youtube.com/watch?v=fBPDk4EtwaM",
+  title: "JNCOs are cool, right?",
+  blurb: "You could put books in your pocket.",
+  description: "Woah bro cool pants they are big!" + loremString,
+  author_id: louis.id,
+  category_id: fashion_category.id,
+  goal: 911,
+  end_date: "2018-01-31 00:00:00.0000",
+  image: File.open('app/assets/images/jnco.jpg')
+)
+
+slow_jams = Campaign.create(
+  video_url: "https://www.youtube.com/watch?v=f4UtHu7RAlE",
+  title: "Bring back slow jams",
+  blurb: "I'm sick of being at the club.  Lets go in my jeep. You remind me of my jeep.",
+  description: "Oh yeah slow jams are comin back.  Its going to be fun times for all!" + loremString,
+  author_id: louis.id,
+  category_id: music_category.id,
+  goal: 9456,
+  end_date: "2018-01-31 00:00:00.0000",
+  image: File.open('app/assets/images/slow-jams.png')
+)
+
+mullet_reward_1 = Reward.create(title: "Mullet Wig!", description: "You get your own mullet wig.  Muscles not included.", campaign_id: mullets_and_muscles.id, delivery_date:"2017-02-31 00:00:00.0000", min_amount: 500)
+mullet_reward_2 = Reward.create(title: "Mullet Wig and WOrkout Video!", description: "You get your own mullet wig.  Muscles are included.", campaign_id: mullets_and_muscles.id, delivery_date:"2017-02-31 00:00:00.0000", min_amount: 5000)
+jnco_reward_1 = Reward.create(title: "Big Pants!", description: "You get your own pair of giant pants to put your skinny legs into!", campaign_id: jncos.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 50)
+
+cheap_reward = Reward.create(
+  title: "Cheapskate award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: slap_bracelets.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 5)
+
+cheap_reward = Reward.create(
+  title: "Cheapskate award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: running_man.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 5)
+
+cheap_reward = Reward.create(
+  title: "Cheapskate award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: jncos.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 5)
+
+cheap_reward = Reward.create(
+  title: "Cheapskate award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: lou_diamond_phillips.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 5)
+
+cheap_reward = Reward.create(
+  title: "Cheapskate award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: running_man.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 5)
+
+
+
+cheap_reward = Reward.create(
+  title: "Awesome award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: slap_bracelets.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 98)
+
+cheap_reward = Reward.create(
+  title: "Awesome award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: running_man.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 98)
+
+cheap_reward = Reward.create(
+  title: "Awesome award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: jncos.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 98)
+
+cheap_reward = Reward.create(
+  title: "Awesome award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: lou_diamond_phillips.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 98)
+
+cheap_reward = Reward.create(
+  title: "Awesome award!",
+  description: "wow f oksdflkjs  ekwelrkj  aflkaskj f lskdfj aslkjasd  gkdkdkfj f fkd f sdflkjasj a slkdjj g g dkjsdflkid d lksdflsdfj sdf eijflksjdf",
+  campaign_id: running_man.id, delivery_date:"2018-01-31 00:00:00.0000", min_amount: 98)
+
 less_cheap_reward = Reward.create(
   title: "You are better than awful",
   description: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec fringilla felis vitae lacinia facilisis. Curabitur rutrum posuere justo. Integer ut tortor orci. Ut rhoncus congue odio a cursus. Maecenas sapien nulla, fermentum eget eros tincidunt, condimentum tempor lacus. Vivamus pulvinar sagittis mattis. Duis diam tellus, lacinia in blandit convallis, molestie vitae enim. Nam mauris justo, mattis sit amet massa id, ultricies porta lacus. Proin velit turpis, suscipit laoreet tempor in, dignissim eget ex. Vivamus et mi quis velit faucibus porttitor. Donec semper nisl finibus ornare gravida.",
@@ -334,3 +439,9 @@ less_cheap_reward = Reward.create(
 cheap_pledge = Pledge.create(reward_id: cheap_reward.id, pledger_id: louis.id, amount:112)
 
 comment1 = Comment.create(author_id: louis.id, campaign_id: slap_bracelets.id, body: "woah this is a cool page dude!", date:DateTime.now)
+comment1 = Comment.create(author_id: louis.id, campaign_id: running_man.id, body: "woah this is a cool page dude!", date:DateTime.now)
+comment1 = Comment.create(author_id: louis.id, campaign_id: jncos.id, body: "woah this is a cool page dude!", date:DateTime.now)
+comment1 = Comment.create(author_id: louis.id, campaign_id: jncos.id, body: "woah this is REALLY a cool page dude!", date:DateTime.now)
+comment1 = Comment.create(author_id: louis.id, campaign_id: lou_diamond_phillips.id, body: "woah this is a cool page dude!", date:DateTime.now)
+comment1 = Comment.create(author_id: louis.id, campaign_id: mullets_and_muscles.id, body: "woah this is a cool page dude!", date:DateTime.now)
+comment1 = Comment.create(author_id: louis.id, campaign_id: running_man.id, body: "I just wanna do it now!", date:DateTime.now)
